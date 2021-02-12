5a8f461e117591b6a50a17ef154c581022d157ad
MickySinghSahni <78903620+MickySinghSahni@users.noreply.github.com>
2021-02-12 22:55:17 +0530

    Add files via upload

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,5 @@
 module.exports = async (client) => {
-  console.log(`[API] Logged in as ${client.user.username}`);
+  console.log(`[Gaara Music API] Logged in as ${client.user.username}`);
   await client.user.setActivity("Music", {
     type: "LISTENING",//can be LISTENING, WATCHING, PLAYING, STREAMING
   });
