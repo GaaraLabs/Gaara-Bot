6a248260ab37eee283282c98f3329a690d837480
MickySinghSahni <78903620+MickySinghSahni@users.noreply.github.com>
2021-02-13 13:26:15 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 module.exports = async (client) => {
   console.log(`[Gaara Music API] Logged in as ${client.user.username}`);
-  await client.user.setActivity("MiCKY#4519 | @Gaara for commands", {
+  await client.user.setActivity("Bot Status Here", {
     type: "WATCHING",//can be LISTENING, WATCHING, PLAYING, STREAMING
   });
 };
